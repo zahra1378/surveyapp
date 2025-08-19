a8d5e17a2c5b4abee7ccab0081f85fb5fbc77c33
unknown <zahra.m346@gmail.com>
2025-08-19 14:32:05 +0330

    change some features in animation of questions

--- src/pages/QuestionPage/QuestionPage.js
+++ src/pages/QuestionPage/QuestionPage.js
@@ -150,10 +150,10 @@ const QuestionPage = () => {
         <AnimatePresence mode="wait">
           <motion.div
             key={questionId}
-            initial={{ opacity: 0, x: 100 }}
+            initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -100 }}
-            transition={{ duration: 0.2 }}
+            exit={{ opacity: 0, x: -50 }}
+            transition={{ duration: 0.3 }}
           >
             {renderQuestion()}
           </motion.div>
